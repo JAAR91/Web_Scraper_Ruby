af7b23e4019799baa8fe99d65064f815ec1a4638
JAAR96 <jaarriagaramos@gmail.com>
2021-04-05 11:21:43 -0600

    add negative test on rpsec for input_checker.rb

--- spec/input_checker_spec.rb
+++ spec/input_checker_spec.rb
@@ -7,11 +7,19 @@ describe Inputchecker do
     it 'checks the input entered and verify its betwen 1 and the parameter entered' do
       expect(input.search_result_check('5', 10)).to eql('5')
     end
+
+    it 'checks the input entered and verify its betwen 1 and the parameter entered' do
+      expect(input.search_result_check('7', 10)).not_to eql('5')
+    end
   end
 
-  describe '#search_result_check' do
-    it 'check for numbers and empty strings' do
+  describe '#empty_checker' do
+    it 'checks if the imput enteres is a empty string or nil' do
       expect(input.empty_checker('Hello World')).to eql('Hello World')
+    end
+
+    it 'checks if the imput enteres is a empty string or nil' do
+      expect(input.empty_checker('I do not know other negative test')).not_to eql('Hello World')
     end
   end
 
@@ -19,23 +27,33 @@ describe Inputchecker do
     it 'checks the input entered its inside the numbers requested' do
       expect(input.menu_list_checker('m', 0, 20, 0, 36)).to eql(['m', 0])
     end
-  end
 
-  describe '#menu_list_checker' do
+    it 'checks the input entered its inside the numbers requested' do
+      expect(input.menu_list_checker('m', 0, 20, 0, 36)).not_to eql(['n', 0])
+    end
+
     it 'checks the input entered if its n and return the next page number' do
       expect(input.menu_list_checker('n', 0, 20, 0, 36)).to eql(['n', 20])
     end
-  end
 
-  describe '#menu_list_checker' do
+    it 'checks the input entered if its n and return the next page number' do
+      expect(input.menu_list_checker('n', 60, 80, 80, 4)).not_to eql(['n', 100])
+    end
+
     it 'checks the input entered if its b and return the previous page number' do
       expect(input.menu_list_checker('b', 0, 20, 40, 36)).to eql(['b', 20])
     end
-  end
 
-  describe '#menu_list_checker' do
+    it 'checks the input entered if its b and return the previous page number' do
+      expect(input.menu_list_checker('b', 0, 0, 20, 36)).not_to eql(['b', -20])
+    end
+
     it 'checks the input entered if its a number betwen the numbers requested' do
       expect(input.menu_list_checker('56', 40, 60, 40, 36)).to eql(['56', 40])
+    end
+
+    it 'checks the input entered if its a number betwen the numbers requested' do
+      expect(input.menu_list_checker('56', 40, 60, 40, 36)).not_to eql(['66', 40])
     end
   end
 
@@ -43,17 +61,30 @@ describe Inputchecker do
     it 'checks the input entered its a number betwen the numbers requested' do
       expect(input.number_checker(11, 0, 20)).to eql(11)
     end
+
+    it 'checks the input entered its a number betwen the numbers requested' do
+      expect(input.number_checker(11, 0, 20)).not_to eql(21)
+    end
   end
 
   describe '#empty_checker' do
     it 'makes sure input is never empty and the returns the input entered' do
       expect(input.empty_checker('hi')).to eql('hi')
     end
+
+    it 'makes sure input is never empty and the returns the input entered' do
+      expect(input.empty_checker('hi')).not_to eql('')
+    end
   end
 
   describe '#checking_array' do
     it 'return an array of numbers as strings and letters b n or m' do
       expect(input.checking_array(20)).to eql(%w[B N b n m M 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38
+                                                 39])
+    end
+
+    it 'return an array of numbers as strings and letters b n or m' do
+      expect(input.checking_array(21)).not_to eql(%w[B N b n m M 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38
                                                  39])
     end
   end
@@ -73,6 +104,14 @@ describe Inputchecker do
 
     it 'returns the new positition of the page when on index' do
       expect(input.back_next('b', 0, 4)).to eql(60)
+    end
+
+    it 'returns the new positition of the page when on index' do
+      expect(input.back_next('b', 0, 4)).not_to eql(-20)
+    end
+
+    it 'returns the new positition of the page when on index' do
+      expect(input.back_next('n', 20, 2)).not_to eql(40)
     end
   end
 end
